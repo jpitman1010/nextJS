43a1f3c39b1b3ff75868188729b8c6f00397e757
jpitman1010 <jpitman1010@gmail.com>
2021-06-19 12:52:19 -0700

    scoped styling example, also variable style included

--- pages/about.js
+++ pages/about.js
@@ -1,11 +1,32 @@
 import Link from 'next/link';
+import Index from './index';
+
+const styles = {
+    color:'blue', 
+    border:'.05rem solid blue', 
+    padding:5
+}
 
 const About = () => (
     <div>
         <h2>About page.</h2>
         <Link href="/">
-            <a style={{color:'blue', border:'.05rem solid blue', padding:5}}>Home</a>
+            <a style={styles}>Home</a>
         </Link>
+        <p>This is p div to show the NextJS styling using style tags <br />
+            which are placed inside of the returned div for page, <br />
+            DO NOT <br />
+            place inside of Link tags, that only supports 1 child...
+        </p>
+        <style jsx>
+            {`
+                p {
+                    color: indigo;
+                    font-size: 20px;
+                    font-weight: light;
+                }           
+            `}
+        </style>
     </div>
 )
 
