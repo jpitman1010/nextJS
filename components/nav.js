9055c303ac03cfd025eba634ec8e85c941c8075e
jpitman1010 <jpitman1010@gmail.com>
2021-06-19 15:03:44 -0700

    loading images

--- components/nav.js
+++ components/nav.js
@@ -2,6 +2,7 @@ import Link from 'next/link';
 
 const Nav = () => (
     <div>
+        <img src="/static/painting.jpg" alt="Julie's painting" height={200} />
         <Link href="/"><a style={{marginRight:10}} href="">Home</a></Link>
         <Link href="/about"><a href="">About</a></Link>
     </div>
