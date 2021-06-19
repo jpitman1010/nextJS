cc36b285c48dd19040c5e421324a162da033c86a
jpitman1010 <jpitman1010@gmail.com>
2021-06-19 13:43:58 -0700

    destructuring the props and simplifying code

--- components/layout.js
+++ components/layout.js
@@ -3,10 +3,10 @@ import Nav from './nav';
 //creating nav component to share the nav across the entire app
 //instead of having to write import Nav for every page.
 //using props for children components to pass the props for each page.
-const Layout = props => (
+const Layout = ({ children }) => (
     <div>
-        <Nav />
-        {props.children}
+        <Nav /> 
+        {children}
     </div>
 );
 
