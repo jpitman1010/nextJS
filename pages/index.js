d84dfd8f86e1dff79304e6ad468c8339b1725fad
jpitman1010 <jpitman1010@gmail.com>
2021-06-19 12:51:44 -0700

    variable styling example

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,20 @@
 import Link from 'next/link';
 
+const styles = {
+    color:'blue', 
+    border:'.05rem solid blue', 
+    padding:5
+}
 
 const Index = () => (
     <div>
         <h2>Hello from NextJS</h2>
         <Link href="/about">
-            <a style={{color:'blue', border:'.05rem solid blue', padding:5}}>About</a>
+            <a style={styles}>About</a>
         </Link>
     </div>
     );
+
+
 
 export default Index;
